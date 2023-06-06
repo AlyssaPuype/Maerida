ce3237df9a1988c4bcdd36cbab6c0789b273763c
alyayee <alyssa.puype@gmail.com>
2023-06-06 12:52:06 +0200

    edit

--- code/script.js
+++ code/script.js
@@ -1,19 +1,8 @@
-const left_dropdown_menu = document.querySelector(".left_dropdown_menu");
-const dropdown = document.querySelector(".dropdown");
-
-left_dropdown_menu.addEventListener("mouseover", () => {
-  dropdown.style.display = "flex";
-});
-
-dropdown.addEventListener("mouseleave", () => {
-  dropdown.style.display = "none";
-});
-
-/* 
-if u want to hide it when clicking on the document, outside the element
-document.addEventListener("click", (event) => {
-    // Check if the clicked element is outside the dropdown and left_dropdown_menu
-    if (!dropdown.contains(event.target) && !left_dropdown_menu.contains(event.target)) {
-      dropdown.style.display = "none";
-    }
-  }); */
+//------TOGGLE MENU
+let navLinks = document.getElementById("navLinks");
+const showMenu = () => {
+    navLinks.style.right = "0";
+}
+const hideMenu = () => {
+    navLinks.style.right = "-200px";
+}
